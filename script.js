0961949f656a4318b0ca7f051be50e75b703297a
RegishShrestha <regishshrestha@gmail.com>
2022-08-27 17:39:31 +0545

    pointer added

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ if (navigator.geolocation) {
     function (position) {
       const { latitude } = position['coords'];
       const { longitude } = position['coords'];
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}z`);
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
       const coords = [latitude, longitude];
 
       const map = L.map('map').setView(coords, 13);
@@ -26,10 +26,27 @@ if (navigator.geolocation) {
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+      //   L.marker(coords)
+      //     .addTo(map)
+      //     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      //     .openPopup();
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('workout')
+          .openPopup();
+      });
     },
     function () {
       alert('Location not found');
